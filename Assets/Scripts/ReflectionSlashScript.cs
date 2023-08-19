2caa61e2c578e576c5cf009e55a218a57101783c
Reno_Home_ZepM16 <renoxiao93@gmail.com>
2023-08-19 22:47:45 +0800

    1-修复无限反弹问题（反弹时射线缩短需要时间的问题；限制单次可反弹次数为4次；）
    2-增加匪dragging删除射线和path
    3-限制enemyboost次数
    4-增加手机touch2次可以切成forward功能

--- Assets/Scripts/ReflectionSlashScript.cs
+++ Assets/Scripts/ReflectionSlashScript.cs
@@ -8,6 +8,9 @@ public class ReflectionSlashScript : MonoBehaviour
     public List<GameObject> endOfPathes; // store all the end of paths in order, updates everyframe
     public List<Vector3> targetPoses; // store all the target poses, only update when mouse lifted
     public List<GameObject> currentlyActivePaths; // store all the active paths
+
+    public int ReflectionTimes = 0;
+
     #region SINGLETON
     public static ReflectionSlashScript me;
     private void Awake()
